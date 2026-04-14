4d0a62ab0226757c518c225ac8b092fe7a87407b
acmccarty01 <143349471+acmccarty01@users.noreply.github.com>
2026-04-13 21:27:02 -0600

    prob 3

--- AircraftEOMDoublet.m
+++ AircraftEOMDoublet.m
@@ -1,4 +1,4 @@
-% Contributors: Victor Turpin Aguayo
+% Contributors: Aidan McCarty
 % Course number: ASEN 3801
 % File name: AircraftEOM
 % Created: 4/7/26
@@ -18,123 +18,18 @@ function xdot = AircraftEOMDoublet(time, aircraft_state, aircraft_surfaces, doub
 % vaiables given values for the previous step and control forces and
 % moments. This function will be used by ode45 to simulate the QR flight. 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%% Assign Useful Names to Variables (x,y,z,phi,theta,etc...)
-
-State.x=var(1); State.y=var(2); State.z=var(3);             % Position
-State.phi=var(4); State.theta=var(5); State.psi=var(6);     % Attitude
-State.u=var(7); State.v=var(8); State.w=var(9);             % Velocity
-State.p=var(10); State.q=var(11); State.r=var(12);          % Angular Rate
-
-
 %% Doublet value 
 if time < doublet_time
-    aircraft_surfaces(1) = aircraft_surfaces(1) + doublet_size;
+    delta_e = aircraft_surfaces(1) + doublet_size;
 elseif time < (2*doublet_time)
-    aircraft_surfaces(1) = aircraft_surfaces(1) - doublet_size;
+    delta_e = aircraft_surfaces(1) - doublet_size;
 else 
-    aircraft_surfaces(1) = aircraft_surfaces(1);
+    delta_e = aircraft_surfaces(1);
 end
 
-%new_aircraft_surfaces = [delta_e, aircraft_surfaces(2:4)];
+new_aircraft_surfaces = [delta_e, aircraft_surfaces(2), aircraft_surfaces(3), aircraft_surfaces(4)]';
 
-%% Call aerodynamics force and moments function
-density = stdatmo(-State.z);
-[aero_forces, aero_moments] = AeroForcesAndMoments(aircraft_state, aircraft_surfaces, wind_inertial, density, aircraft_parameters);
-Aero.X = aero_forces(1); Aero.Y = aero_forces(2); Aero.Z = aero_forces(3); 
-Aero.L = aero_moments(1); Aero.M = aero_moments(2); Aero.N = aero_moments(3); 
-
-%% Set Up Euler Angle Structure 
-
-% Calculate Trig values of Psi
-Trig.cpsi = cos(State.psi); Trig.spsi = sin(State.psi);
-Trig.tpsi = tan(State.psi);
-
-% Calculate Trig values of Theta
-Trig.ctheta = cos(State.theta); Trig.stheta = sin(State.theta);
-Trig.ttheta = tan(State.theta);
-
-% Calculate Trig values of Phi
-Trig.cphi = cos(State.phi); Trig.sphi = sin(State.phi);
-Trig.tphi = tan(State.phi);
-
-%% Extracting Inertia Values
-
-In.x = aircraft_parameters.Ix; In.y = aircraft_parameters.Iy; In.z = aircraft_parameters.Iz;
-In.xz = aircraft_parameters.Ixz;
-
-%% X_dot, Y_dot, Z_dot
-
-% Calculate d/dt[Inertial Position]
-Pos_dot=[Trig.ctheta.*Trig.cpsi,... % Calculate B to E DCM
-    (Trig.sphi.*Trig.stheta.*Trig.cpsi)-(Trig.cphi.*Trig.spsi),...
-    (Trig.cphi.*Trig.stheta.*Trig.cpsi)-(Trig.sphi.*Trig.spsi);...
-    (Trig.ctheta.*Trig.spsi),...
-    (Trig.sphi.*Trig.stheta.*Trig.spsi)+(Trig.cphi.*Trig.cpsi),...
-    (Trig.cphi.*Trig.stheta.*Trig.spsi)-(Trig.sphi.*Trig.cpsi);...
-    (-Trig.stheta),...
-    (Trig.ctheta.*Trig.sphi),...
-    (Trig.ctheta.*Trig.cphi)] * [State.u;State.v;State.w];
-
-% Allocate d/dt[Inertial Position]
-X_dot = Pos_dot(1);
-Y_dot = Pos_dot(2);
-Z_dot = Pos_dot(3);
-
-
-%% Phi_dot, Theta_dot, Psi_dot
-
-% Calculate d/dt[Attiude]
-Angle_dot = [1, Trig.sphi.*Trig.ttheta, Trig.cphi.*Trig.ttheta;
-             0,        Trig.cphi,             -Trig.sphi      ;
-             0, Trig.sphi./Trig.ctheta, Trig.cphi./Trig.ctheta]...
-            *[State.p;State.q;State.r]; 
-
-
-% Allocate d/dt[Attiude]
-Phi_dot = Angle_dot(1); Theta_dot = Angle_dot(2); Psi_dot = Angle_dot(3);
-
-%% U_dot, V_dot, W_dot
-
-% Calculate d/dt[Velocity]
-v_dot = cross([State.u,State.v,State.w],[State.p,State.q,State.r])' ...
-    + g.*[-Trig.stheta;Trig.ctheta.*Trig.sphi ;Trig.ctheta.*Trig.cphi] ...
-    +[Aero.X; Aero.Y; Aero.Z]./m;
-% +[0;0;Control.Z]./m;
-
-% ALlocate d/dt[Velocity]
-U_dot = v_dot(1); V_dot = v_dot(2); W_dot = v_dot(3);
-
-
-%% P_dot, Q_dot, R_dot
-
-% Calculate d/dt[Angular Rate]
-Gamma = In.x * In.z - (In.xz)^2;
-Gamma1 = In.xz * (In.x - In.y + In.z) / Gamma;
-Gamma2 = (In.z * (In.z - In.y) + (In.xz)^2) / Gamma;
-Gamma3 = In.z / Gamma;
-Gamma4 = In.xz / Gamma;
-Gamma5 = (In.z - In.x) / In.y;
-Gamma6 = In.xz / In.y;
-Gamma7 = (In.x * (In.x - In.y) + (In.xz)^2) / Gamma;
-Gamma8 = In.x / Gamma;
-
-Omega_dot = [ Gamma1*State.p*State.q - Gamma2*State.q.*State.r ;...
-              Gamma5*State.p*State.r - Gamma6.*(State.p^2 - State.r^2) ;...
-              Gamma7*State.p*State.q - Gamma1.*State.q.*State.r ]...
-           + [ Gamma3*Aero.L + Gamma4.*Aero.N ;...
-              Aero.M/In.y;...
-              Gamma4*Aero.L + Gamma8.*Aero.N  ];
-           
-% +[ (1/In.x).*Control.L ;...
-%               (1/In.y).*Control.M ;...
-%               (1/In.z).*Control.N];
-
-% Allocate d/dt[Angular Rate]
-P_dot = Omega_dot(1); Q_dot = Omega_dot(2); R_dot = Omega_dot(3);
-
-%% Compile All Dotted States
-
-xdot=[ X_dot; Y_dot; Z_dot; Phi_dot; Theta_dot; Psi_dot; U_dot; V_dot; W_dot; P_dot; Q_dot; R_dot];
+xdot = AircraftEOM(time, aircraft_state, new_aircraft_surfaces, wind_inertial, aircraft_parameters);
 
 end
+
--- P3main.m
+++ P3main.m
@@ -5,22 +5,29 @@
 clc
 clear
 close all
+ttwistor();
 
-% Case 3
-var_0 = [0 0 -1800
-        deg2rad(15) deg2rad(-12) deg2rad(270)
-        19 3 -2
-        deg2rad(0.08) deg2rad(-0.2) 0]'; 
-aircraft_surfaces = [deg2rad(5) deg2rad(2) deg2rad(-13) 0.3]';
+% Case 2
+var_0 = [0 0 -1800 0 0.0278 0 20.99 0 0.5837 0 0 0]'; 
+
+aircraft_surfaces = [0.1079 0 0 0.3182]';
 wind_inertial = [0 0 0]';
 
 doublet_time = 0.25;
 doublet_size = deg2rad(15);
 
+%% 3.2
 tspan = 0:0.01:3;
 
-
-[time, x] = ode45(@(time,aircraft_state) AircraftEOMDoublet(time, var_0, aircraft_surfaces, doublet_size, ...
-    doublet_time, wind_inertial, aircraft_parameters), tspan, var_0);
-
+ode_func1 = @(time,aircraft_state) AircraftEOMDoublet(time, var_0, aircraft_surfaces, doublet_size, ...
+    doublet_time, wind_inertial, aircraft_parameters);
+[time,x] = ode45(ode_func1,tspan,var_0);
 PlotAircraftSim(time,x,aircraft_surfaces,[1 2 3 4 5 6],['b', 'b', 'b', 'b', 'b', 'b', 'k']')
+
+%% 3.3
+% tspan = 0:0.1:100;
+% ode_func2 = @(time,aircraft_state) AircraftEOMDoublet(time, var_0, aircraft_surfaces, doublet_size, ...
+%     doublet_time, wind_inertial, aircraft_parameters);
+% [time,x] = ode45(ode_func2,tspan,var_0);
+% 
+% PlotAircraftSim(time,x,aircraft_surfaces,[1 2 3 4 5 6],['b', 'b', 'b', 'b', 'b', 'b', 'k']')
